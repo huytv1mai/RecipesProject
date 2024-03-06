e90e4f2ea257d8aecb3dc6e349851316dc060362
Huy <tranvuhuy2510@gmail.com>
2024-03-06 16:11:05 +0700

    fix register

--- JamesThewWebMVC/wwwroot/client/js/main.js
+++ JamesThewWebMVC/wwwroot/client/js/main.js
@@ -278,12 +278,12 @@
 	const signInButton = document.getElementById('signIn');
 	const container = document.getElementById('container');
 
-	//signUpButton.addEventListener('click', () => {
-	//	container.classList.add("right-panel-active");
-	//});
+	signUpButton.addEventListener('click', () => {
+		container.classList.add("right-panel-active");
+	});
 
-	//signInButton.addEventListener('click', () => {
-	//	container.classList.remove("right-panel-active");
-	//});
+	signInButton.addEventListener('click', () => {
+		container.classList.remove("right-panel-active");
+	});
 })(jQuery);
 
